d3ca3ed65f0222c5fbf7d764de0237ffef72d741
AUDIY <96096729+AUDIY@users.noreply.github.com>
2023-09-18 23:37:52 +0900

    Add driver for ACM0802C-NLW-BBW-IIC
    includes device datasheets.

---
+++ 01_ACM0802C-NLW-BBW-IIC/acm0802c_i2c.c
@@ -0,0 +1,94 @@
+/*
+ * acm0802c_i2c.c
+ *
+ *  Created on: Sep 10, 2023
+ *      Author: AUDIY
+ */
+
+#include "acm0802c_i2c.h"
+#include "stm32f4xx_hal.h"
+#include <string.h>
+
+
+/* Device Address Search */
+uint16_t CheckACMDeviceAddress(I2C_HandleTypeDef *hi2c) {
+    int i = 0;
+    HAL_StatusTypeDef s;
+    uint16_t AD[4] = {ACM0802C_AD0, ACM0802C_AD1, ACM0802C_AD2, ACM0802C_AD3}; // 0x78, 0x7A, 0x7C, 0x7E
+
+    for (i = 0; i < 4; i++) {
+        s = HAL_I2C_IsDeviceReady(hi2c, AD[i], 10, 1000);
+        
+        if (s == HAL_OK) {
+            // if HAL_OK is returned, break this loop. 
+            break;
+        }
+    }
+
+    // Return the Address which meets s == HAL_OK.
+    return (s == HAL_OK) ? AD[i] : 0x00;
+}
+
+/* Write Instruction */
+uint8_t ACMWriteInstruction(I2C_HandleTypeDef *hi2c, uint16_t AD, uint8_t data) {
+    uint8_t buf[2] = {INST_C0_0, data}; // 0x00, data
+    uint8_t status = HAL_I2C_Master_Transmit(hi2c, AD, buf, 2, 1000);
+
+    return status == HAL_OK;
+}
+
+/* Initialize ACM0802C-NLW-BBW-I2C */
+void InitACM(I2C_HandleTypeDef *hi2c, uint16_t AD) {
+    /* Wait for more than 40 ms after VDD rises to 4.5V */
+    HAL_Delay(50);
+
+    ACMWriteInstruction(hi2c, AD, FUNC_SET_8_2_8); // Function Set: 8-bit/2-line/5x8
+    HAL_Delay(1); // wait for more than 100 us
+
+    ACMWriteInstruction(hi2c, AD, DISP_DCB_OFF);   // Display ON/OFF:  B/C/D = OFF/OFF/OFF
+    HAL_Delay(1); // wait for more than 100 us
+
+    ACMWriteInstruction(hi2c, AD, CLR_DISP);       // Clear Display.
+    HAL_Delay(20); // wait for more than 10 ms
+
+    ACMWriteInstruction(hi2c, AD, ENTRY_KEEP);     // Entry Mode Set: Shift is NOT performed
+    HAL_Delay(1); // wait for more than 100 us
+}
+
+/* Print Single Character to the ACM0802C-NLW-BLW-I2C. */
+uint8_t ACMPrintChar(I2C_HandleTypeDef *hi2c, uint16_t AD, const char *cdata) {
+    uint8_t buf[2] = {WRITE_C0_0, *cdata}; // 0x40, *cdata
+    uint8_t status = HAL_I2C_Master_Transmit(hi2c, AD, buf, 2, 1000);
+    HAL_Delay(10);
+
+    return status == HAL_OK;
+}
+
+/* Print 1-Line String (under 40 characters) to the ACM0802C-NLW-BLW-I2C. */
+void ACMPrintStr(I2C_HandleTypeDef *hi2c, uint16_t AD, uint8_t LN, const char *str) {
+    uint8_t LNx = DDRAM_LN1;
+    uint8_t str_len = 0;
+    uint8_t i = 0;
+    const char *startstr = str;
+
+    if (LN == 1) {
+        LNx = DDRAM_LN1; // DDRAM Address: 0x80
+    } else {
+        LNx = DDRAM_LN2; // DDRAM Address: 0xC0
+    }
+
+    str_len = strlen(str); // Count String Length.
+
+    if (str_len > 40) {
+        str_len = 40; // String Length must be under 40 per Line.
+    }
+
+    /* Print Characters in the string */
+    for (i = 0; i < str_len; i++) {
+        ACMWriteInstruction(hi2c, AD, (LNx | i)); // Specify the DDRAM Address
+        ACMPrintChar(hi2c, AD, str); // Write the character the DDRAM.
+        str++;
+    }
+
+    str = startstr; // Return *str pointer to start address.
+}
---
+++ 01_ACM0802C-NLW-BBW-IIC/acm0802c_i2c.h
@@ -0,0 +1,235 @@
+/*
+ * acm0802c_i2c.h
+ *
+ *  Created on: Sep 10, 2023
+ *      Author: AUDIY
+ */
+
+#ifndef INC_ACM0802C_I2C_H_
+#define INC_ACM0802C_I2C_H_
+
+#include <stdint.h>
+#include "stm32f4xx_hal.h"
+
+
+/* Begin Constant Definition */
+/*-----------------------------------------------------------------------------
+* ACM0802C-NLW-BBW-I2C Device Address
+*
+* Device Address (bin): 0b01111[SA1][SA0]0
+* Device Address (hex): 0x7[8|A|C|E]
+* 
+* SA1/SA0 depends on the connection.
+* When No.4/No.5 pin is pulled-up by Vdd: SA1(No.4)/SA0(No.5) is 0b1.
+* When No.4/No.5 pin is pulled_down     : SA1(No.4)/SA0(No.5) is 0b0.
+-----------------------------------------------------------------------------*/
+#define ACM0802C_AD0 0x78 // No.4(SA1) = pulled-down, No.5(SA0) = pulled-down: 0b01111000
+#define ACM0802C_AD1 0x7A // No.4(SA1) = pulled-down, No.5(SA0) = pulled-up  : 0b01111010
+#define ACM0802C_AD2 0x7C // No.4(SA1) = pulled-up  , No.5(SA0) = pulled-down: 0b01111100
+#define ACM0802C_AD3 0x7E // No.4(SA1) = pulled-up  , No.5(SA0) = pulled-up  : 0b01111110
+
+
+/*-----------------------------------------------------------------------------
+* ACM0802C-NLW-BBW-I2C Register Address
+*
+* 1. Instruction
+* Instruction Address (bin): 0b[C0]0000000
+* Instruction Address (hex): 0x[0|8]0
+*
+* 2. Write characters
+* Write Operation Address (bin): 0b[C0]1000000
+* Write Operation Address (hex): 0x[4|C]0
+*
+* C0 bit Setting.
+* C0 = 0: Last control byte to be sent. Only a stream of data bytes is allowed to follow.
+*         This stream may only be terminated by s STOP or RE-START condition.
+* C0 = 1: Another control byte will follow the data byte unless a STOP or RE-START condition is received.
+-----------------------------------------------------------------------------*/
+#define INST_C0_0  0x00 // 0b00000000
+#define INST_C0_1  0x80 // 0b10000000
+#define WRITE_C0_0 0x40 // 0b01000000
+#define WRITE_C0_1 0xC0 // 0b11000000
+
+
+/*-----------------------------------------------------------------------------
+* ACM0802C-NLW-BLW-I2C Instruction Sets
+*
+* ACM0802C has following instructions
+*  1. Read Display Data
+*  2. Write Display Data
+*  3. Clear Display
+*  4. Return Home
+*  5. Entry Mode Set
+*  6. Display ON/OFF
+*  7. Cursor or Display Shift
+*  8. Function Set
+*  9. Set CGRAM Address
+* 10. Set DDRAM Address
+* 11. Read Busy Flag & Address
+-----------------------------------------------------------------------------*/
+/* 1. Read Display Data */
+// Undefined
+
+/* 2. Write Display Data */
+// Undefined
+
+/* 3. Clear Display */
+#define CLR_DISP 0x01 // 0b00000001
+
+/* 4. Return Home */
+#define RTN_HOME 0x20 // 0b00000010
+
+/* 5. Entry Mode Set
+*  Data (bin): 0b000001[I/D][S]
+*  Data (hex): 0x0[4|5|7]
+*
+*  I/D: Increment/Decrement Bit
+*  0b0: cursor/blink moves to left and DDRAM address is decreased by 1.
+*  0b1: cursor/blink moves to right and DDRAM address is increased by 1.
+*
+*  S: Shift of Entire Display
+*  0b0: Shift of entire display is not performed.
+*  0b1: shift of entire display is performed according to I/D value (I/D = "1": shift left, I/D = "0": shift right).
+*/
+#define ENTRY_KEEP  0x04 // Shift is NOT performed: 0b00000100
+#define ENTRY_RIGHT 0x05 // Right Shift           : 0b00000101
+#define ENTRY_LEFT  0x07 // Left Shift            : 0b00000111
+
+/* 6. Display ON/OFF
+*  Data (bin): 0b00001[D][C][B]
+*  Data (hex): 0b0[8-F]
+* 
+*  D:  Display ON/OFF control bit.
+*  0b0: display is turned off, but display data is remained in DDRAM.
+*  0b1: entire display is turned on.
+*
+*  C: Cursor ON/OFF control bit.
+*  0b0: cursor is disappeared in current display, but I/D register remains its data.
+*  0b1: cursor is turned on.
+*
+*  B: Cursor Blink ON/OFF control bit.
+*  0b0: blink is off.
+*  0b1: cursor blink is on, that performs alternate between all the high data and display character at the cursor position.
+*       If fosc has 540 kHz frequency, blinking has 185 ms interval. 
+*/
+#define DISP_DCB_OFF 0x08 // D/C/B = OFF/OFF/OFF: 0b00001000
+#define DISP_B_ON    0x09 // D/C/B = OFF/OFF/ON : 0b00001001
+#define DISP_C_ON    0x0A // D/C/B = OFF/ON/OFF : 0b00001010
+#define DISP_CB_ON   0x0B // D/C/B = OFF/ON/ON  : 0b00001011
+#define DISP_D_ON    0x0C // D/C/B = ON/OFF/OFF : 0b00001100
+#define DISP_DB_ON   0x0D // D/C/B = ON/OFF/ON  : 0b00001101
+#define DISP_DC_ON   0x0E // D/C/B = ON/ON/OFF  : 0b00001110
+#define DISP_DCB_ON  0x0E // D/C/B = ON/ON/ON   : 0b00001111
+
+/* 7. Cursor or Display Shift
+*  Data (bin): 0b0001[S/C][R/L]00
+*  Data (hex): 0x1[0|4|8|C]
+*
+*  S/C: Cursor or Display Shift.
+*  0b0: Cursor shift.
+*  0b1: Display shift.
+*
+*  R/L: Right/Left Shift.
+*  0b0: Shift to Left.
+*  0b1: Shift to Right.
+*/
+#define CURSOR_SHIFT_LEFT  0x10 // S/C = Cursor , R/L = Left : 0b00010000
+#define CURSOR_SHIFT_RIGHT 0x14 // S/C = Cursor , R/L = Right: 0b00010100
+#define DISP_SHIFT_LEFT    0x18 // S/C = Display, R/L = Left : 0b00011000
+#define DISP_SHIFT_RIGHT   0x1C // S/C = Display, R/L = Right: 0b00011100
+
+/* 8. Function Set
+*  Data (bin): 0b001[DL][N][F]00
+*  Data (hex): 0x[2|3][4|8|C]
+*
+*  DL: Interface data length control bit.
+*  0b0: 4-bit interface.
+*  0b1: 8-bit/I2C/SPI interface.
+*
+*  N: Display line number control bit.
+*  0b0: 1-line display mode.
+*  0b1: 2-line display mode.
+*
+*  F: Display font type control bit.
+*  0b0: 5x8  dot format.
+*  0b1: 5x11 dot format.
+*/
+#define FUNC_SET_4_1_8  0x20 // DL/N/F = 4-bit/1-line/5x8 : 0b00100000
+#define FUNC_SET_4_1_11 0x24 // DL/N/F = 4-bit/1-line/5x11: 0b00100100
+#define FUNC_SET_4_2_8  0x28 // DL/N/F = 4-bit/2-line/5x8 : 0b00101000
+#define FUNC_SET_4_2_11 0x2C // DL/N/F = 4-bit/2-line/5x11: 0b00101100
+#define FUNC_SET_8_1_8  0x30 // DL/N/F = 8-bit/1-line/5x8 : 0b00110000
+#define FUNC_SET_8_1_11 0x34 // DL/N/F = 8-bit/1-line/5x11: 0b00110100
+#define FUNC_SET_8_2_8  0x38 // DL/N/F = 8-bit/2-line/5x8 : 0b00111000
+#define FUNC_SET_8_2_11 0x3C // DL/N/F = 8-bit/2-line/5x11: 0b00111100
+
+/* 9. Set CGRAM Address */
+// Undefined
+
+/* 10. Set DDRAM Address 
+*  Data (bin): 0b1[AC6][AC5][AC4][AC3][AC2][AC1][AC0]
+*  Data (hex): 0x[80 - A7|C0 - E7]
+*
+*  How to define address.
+*  Address = DDRAM_LNx | [00 - 27]
+*  Ex. 0x00 = DDRAM_LN1 | 0x00
+*      0x27 = DDRAM_LN1 | 0x27
+*      0x42 = DDRAM_LN2 | 0x02
+*      0x67 = DDRAM_LN2 | 0x27
+*
+*  (1). Display Position
+*  |00|01|02|03|04|05|06|07|
+*  |40|41|42|43|44|45|46|47|
+*
+*  (2). For Shift Left
+*  |01|02|03|04|05|06|07|08|
+*  |41|42|43|44|45|46|47|48|
+*
+*  (3). For Shift Right
+*  |27|00|01|02|03|04|05|06|
+*  |67|40|41|42|43|44|45|46|
+*/
+#define DDRAM_LN1 0x80 // 0b10[AC5][AC4][AC3][AC2][AC1][AC0]
+#define DDRAM_LN2 0xC0 // 0b11[AC5][AC4][AC3][AC2][AC1][AC0]
+
+
+/* 11. Read Busy Flag & Address */
+// Undefined
+
+/*End Constant Definition*/
+
+
+/* Begin Prototype Declaration*/
+/*-----------------------------------------------------------------------------
+* CheckACMDeviceAddress
+* Search & Return the Active I2C Device Address.
+-----------------------------------------------------------------------------*/
+uint16_t CheckACMDeviceAddress(I2C_HandleTypeDef *hi2c);
+
+/*-----------------------------------------------------------------------------
+* ACMWriteInstruction
+* Transmit an instruction set to the ACM0802C-NLW-BLW-I2C.
+-----------------------------------------------------------------------------*/
+uint8_t ACMWriteInstruction(I2C_HandleTypeDef *hi2c, uint16_t AD, uint8_t data);
+
+/*-----------------------------------------------------------------------------
+* InitACM
+* Initialize ACM0802C-NLW-BBW-I2C.
+-----------------------------------------------------------------------------*/
+void InitACM(I2C_HandleTypeDef *hi2c, uint16_t AD);
+
+/*-----------------------------------------------------------------------------
+* ACMPrintChar
+* Print Single Character to the ACM0802C-NLW-BLW-I2C.
+-----------------------------------------------------------------------------*/
+uint8_t ACMPrintChar(I2C_HandleTypeDef *hi2c, uint16_t AD, const char *data);
+
+/*-----------------------------------------------------------------------------
+* ACMPrintStr
+* Print 1-Line String (under 40 characters) to the ACM0802C-NLW-BLW-I2C.
+-----------------------------------------------------------------------------*/
+void ACMPrintStr(I2C_HandleTypeDef *hi2c, uint16_t AD, uint8_t LN, const char *str);
+
+/* End Prototype Declaration */
+
+#endif /* INC_ACM0802C_I2C_H_ */
